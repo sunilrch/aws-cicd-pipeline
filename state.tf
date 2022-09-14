ecebcb23986af6ef71992b6b113413b19c65733f
Sunilchaudh <v1i83sc@sph.com.sg>
2022-09-14 22:14:06 +0530

    added config

--- state.tf
+++ state.tf
@@ -1,12 +1,12 @@
-terraform{
-    backend "s3" {
-        bucket = "sunil-aws-cicd-pipeline"
-        encrypt = true
-        key = "terraform.tfstate"
-        region = "ap-south-1"
-    }
+terraform {
+  backend "s3" {
+    bucket  = "sunil-aws-cicd-pipeline"
+    encrypt = true
+    key     = "terraform.tfstate"
+    region  = "ap-south-1"
+  }
 }
 
 provider "aws" {
-    region = "ap-south-1"
+  region = "ap-south-1"
 }
--- terraform.tfvars
+++ terraform.tfvars
@@ -1,2 +1,2 @@
-dockerhub_credentials = "arn:aws:secretsmanager:ap-south-1:964761116607:secret:codebuild/dockerhub-EWY7us"
+dockerhub_credentials          = "arn:aws:secretsmanager:ap-south-1:964761116607:secret:codebuild/dockerhub-EWY7us"
 codestar_connector_credentials = "arn:aws:codestar-connections:ap-south-1:964761116607:connection/786b4207-ff33-4fd3-97f2-aeb029fedc7c"
